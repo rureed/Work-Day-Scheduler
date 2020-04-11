edd64a6760446101dc27b8e7815aeb221890bca4
rureed <rureed@gmail.com>
2020-04-11 00:25:37 -0400

    updating files

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 
 $(document).ready(function() {
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+    console.log(currentDay)
+
+    function colorCode() {
+        var now = moment().hours()
+    
+    }
+
+
+
 })
 
-console.log(currentDay)
 
-function colorCode() {
-    var now = moment().hours()
-    
-}
+$(".saveBtn").on("click", function() {
+    var entry = $(this).parent().attr("id");
+    var data = $("textarea").val();
+    localStorage.setItem(entry, data);
+})
 
+$("#9am").children("textarea").val(localStorage.getItem("9am"));
