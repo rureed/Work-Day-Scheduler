8dc9ccd2aa8b8149bc112592b1289bd4eb5dc10d
rureed <rureed@gmail.com>
2020-04-11 23:51:30 -0400

    updating files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# hw5
+Russ Reed's Work Day Scheduler
+
+This is a simple display of work hours from 9am to 5pm.
+
+In the middle of each "hour row", the user can input data and save it with the save button at the right end of each row, which saves the data to localStorage. When the page is refreshed, the data persists. Each save button only saves the data in its own row.
+
+This scheduler does not color code the rows according to the time of day (past/present/future).
--- script.js
+++ script.js
@@ -5,18 +5,18 @@ $(document).ready(function() {
 
     console.log(currentDay)
 
-    function colorCode() {
-        var now = m.hours()
-    
+    // function colorCode() {
+    //     var now = m.hours()
+    // I puzzled over this for some time, but ran out of time to complete the color coding.
 
-    }
+    });
 
     $(".saveBtn").on("click", function() {
         var time = $(this).parent().attr("id");
         var data = $(this).siblings("input").val();
         localStorage.setItem(time, data);
         console.log(time, data);
-    });
+
     
       $("#9").children("input").val(localStorage.getItem("9"));
       $("#10").children("input").val(localStorage.getItem("10"));
@@ -28,29 +28,9 @@ $(document).ready(function() {
       $("#4").children("input").val(localStorage.getItem("4"));
       $("#5").children("input").val(localStorage.getItem("5"));
 
-      var timeOfday = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
-      updatetime();
-      
-      function updatetime() {
-        var currentTime = moment().format('H');
-        for(var i = 0; i < timeOfday.length; i++) {
-      
-          if (parseInt(timeOfday[i]) > currentTime) {
-            $("#" + timeOfday[i]).attr("style", "background-color: #58ce7b");      
-      
-          }
-          else if (parseInt(timeOfday[i]) < currentTime) {
-            $("#" + timeOfday[i]).attr("style", "background-color: lightgray");
-      
-          }
-          else if (parseInt(timeOfday[i]) == currentTime) {
-            $("#" + timeOfday[i]).attr("style", "background-color: #fc665e");
-          
-          }
-        }
-      }
+    });
 
-    })
+    
 
 
 
